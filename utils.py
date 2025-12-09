035089f980328f095afd0d2820adc40743ef85d2
farhan-exe <farhanvai4070@gmail.com>
2025-12-09 03:30:17 -0600

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,44 @@
+import aiohttp
+import os
+from dotenv import load_dotenv
+import asyncio 
+
+load_dotenv()
+
+
+async def check_ban(uid: str) -> dict | None:
+    api_url = f"http://raw.thug4ff.com/check_ban/{uid}"
+
+    timeout = aiohttp.ClientTimeout(total=10)
+
+    try:
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.get(api_url) as response:
+
+                response.raise_for_status()
+
+                response_data = await response.json()
+
+                if response_data.get("status") == 200:
+                    data = response_data.get("data")
+                    if data:
+                        return {
+                            "is_banned": data.get("is_banned", 0),
+                            "nickname": data.get("nickname", ""),
+                            "period": data.get("period", 0),
+                            "region": data.get("region", 0)
+                        }
+
+                return None
+
+    except aiohttp.ClientError as e:
+        print(f"API request failed for UID {uid}: {e}")
+        return None
+
+    except asyncio.TimeoutError:
+        print(f"API request timed out for UID {uid}.")
+        return None
+
+    except Exception as e:
+        print(f"Unexpected error for UID {uid}: {e}")
+        return None
